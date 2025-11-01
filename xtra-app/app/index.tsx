e8fe90a88a9adf96f207330942a6a0824d842cd9
charlie <sxergiu@gmail.com>
2025-11-01 11:29:41 +0200

    reset template project and init blank node server

---
+++ xtra-app/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Connect your Instagram account with Canva, then login to Xtra.</Text>
+    </View>
+  );
+}
